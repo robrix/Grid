496e20a519a227794e64371606aca9126aea5bd9
Rob Rix <rix.rob@gmail.com>
2009-05-23 08:38:01 -0400

    Initial import.

---
+++ Other Sources/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Grid
+//
+//  Created by Rob Rix on 23/05/09.
+//  Copyright Monochrome Industries 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
