6e8c3621bc47febfc149ce64f093711670ca830a
Rob Rix <rix.rob@gmail.com>
2009-05-27 02:05:07 -0400

    Drawing changes. We now draw the view bounds brightly. We now draw the selected fraction intervals brightly. We now draw the selection area below the view bounds, above the fraction intervals, and aligned to solid pixels.

--- View/GRAreaSelectionView.m
+++ View/GRAreaSelectionView.m
@@ -11,6 +11,7 @@
 -(void)drawRect:(NSRect)rect {
 	[[NSColor colorWithDeviceWhite: 1.0f alpha: 0.25f] setStroke];
 	
+	// inactive horizontal fractions
 	NSBezierPath *path = [NSBezierPath bezierPath];
 	for(NSUInteger fraction = 1; fraction <= self.delegate.maximumHorizontalFractions; fraction++) {
 		for(NSUInteger n = 1; n < fraction; n++) {
@@ -20,6 +21,7 @@
 	}
 	[path stroke];
 	
+	// inactive vertical fractions
 	path = [NSBezierPath bezierPath];
 	for(NSUInteger fraction = 1; fraction <= self.delegate.maximumVerticalFractions; fraction++) {
 		for(NSUInteger n = 1; n < fraction; n++) {
@@ -29,24 +31,42 @@
 	}
 	[path stroke];
 	
-	path = [NSBezierPath bezierPathWithRect: NSInsetRect(self.bounds, 0.5, 0.5)];
-	[path stroke];
 	
 	[[[NSColor colorForControlTint: [NSColor currentControlTint]] blendedColorWithFraction: 0.1f ofColor: [NSColor clearColor]] setFill];
-	[[[[NSColor colorForControlTint: [NSColor currentControlTint]] blendedColorWithFraction: 0.1f ofColor: [NSColor clearColor]] blendedColorWithFraction: 0.5f ofColor: [NSColor whiteColor]] setStroke];
+	[[NSColor whiteColor] setStroke];
 	
+	// active horizontal fractions
+	path = [NSBezierPath bezierPath];
+	for(NSUInteger n = 1; n < self.delegate.horizontalSelectedFraction; n++) {
+		[path moveToPoint: NSMakePoint(roundf((self.bounds.size.width / self.delegate.horizontalSelectedFraction) * n) + 0.5f, 0)];
+		[path lineToPoint: NSMakePoint(roundf((self.bounds.size.width / self.delegate.horizontalSelectedFraction) * n) + 0.5f, NSHeight(self.bounds))];
+	}
+	[path stroke];
+	
+	// active vertical fractions
+	path = [NSBezierPath bezierPath];
+	for(NSUInteger n = 1; n < self.delegate.verticalSelectedFraction; n++) {
+		[path moveToPoint: NSMakePoint(0, roundf((self.bounds.size.height / self.delegate.verticalSelectedFraction) * n) + 0.5f)];
+		[path lineToPoint: NSMakePoint(NSWidth(self.bounds), roundf((self.bounds.size.height / self.delegate.verticalSelectedFraction) * n) + 0.5f)];
+	}
+	[path stroke];
+	
+	// selected area
 	NSSize fractionSize = NSMakeSize(
-		roundf(self.bounds.size.width / self.delegate.horizontalSelectedFraction) + 0.5f,
-		roundf(self.bounds.size.height / self.delegate.verticalSelectedFraction) + 0.5f
+		self.bounds.size.width / self.delegate.horizontalSelectedFraction,
+		self.bounds.size.height / self.delegate.verticalSelectedFraction
 	);
 	NSRange horizontalSelectedFractionRange = self.delegate.horizontalSelectedFractionRange, verticalSelectedFractionRange = self.delegate.verticalSelectedFractionRange;
 	NSRect selectedArea = NSMakeRect(
-		fractionSize.width * horizontalSelectedFractionRange.location, fractionSize.height * verticalSelectedFractionRange.location,
-		fractionSize.width * horizontalSelectedFractionRange.length, fractionSize.height * verticalSelectedFractionRange.length
+		roundf(fractionSize.width * horizontalSelectedFractionRange.location) + 0.5f, roundf(fractionSize.height * verticalSelectedFractionRange.location) + 0.5f,
+		roundf(fractionSize.width * horizontalSelectedFractionRange.length), roundf(fractionSize.height * verticalSelectedFractionRange.length)
 	);
 	path = [NSBezierPath bezierPathWithRect: selectedArea];
 	[path fill];
 	[path stroke];
+	
+	// view bounds
+	[[NSBezierPath bezierPathWithRect: NSInsetRect(self.bounds, 0.5, 0.5)] stroke];
 }
 
 @end
