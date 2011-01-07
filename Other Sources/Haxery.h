93cd76fee4d1a4113d251263c0f78e6bb09d9392
Rob Rix <rob@monochromeindustries.com>
2011-01-06 21:13:27 -0500

    Added a Haxery.h file.

---
+++ Other Sources/Haxery.h
@@ -0,0 +1,8 @@
+// Haxery.h
+// Created by Rob Rix on 2011-01-06
+// Copyright 2011 Monochrome Industries
+
+#import <Haxery/HAXElement.h>
+#import <Haxery/HAXSystemWideElement.h>
+#import <Haxery/HAXApplicationElement.h>
+#import <Haxery/HAXWindowElement.h>
