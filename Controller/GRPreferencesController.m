ca122ca4de71128478e58fa15e2858eb5d8a5a9b
Rob Rix <rob.rix@me.com>
2011-09-30 08:14:05 -0400

    The status item’s menu is complete.

--- Controller/GRPreferencesController.m
+++ Controller/GRPreferencesController.m
@@ -35,7 +35,6 @@ OSStatus GRShortcutWasPressed(EventHandlerCallRef nextHandler, EventRef event, v
 			[NSNumber numberWithInteger:50], @"keyCode",
 			[NSNumber numberWithUnsignedInteger:cmdKey + optionKey], @"modifierFlags",
 		nil], @"GRShortcut",
-		[NSNumber numberWithBool:YES], @"GRShowInMenuBar",
 	nil]];
 }
 
@@ -66,10 +65,9 @@ OSStatus GRShortcutWasPressed(EventHandlerCallRef nextHandler, EventRef event, v
 }
 
 -(void)setShortcut:(NSDictionary *)shortcut {
+	[[NSUserDefaults standardUserDefaults] setObject:shortcut forKey:@"GRShortcut"];
+	[[NSUserDefaults standardUserDefaults] synchronize];
 	if(shortcut) {
-		[[NSUserDefaults standardUserDefaults] setObject:shortcut forKey:@"GRShortcut"];
-		[[NSUserDefaults standardUserDefaults] synchronize];
-		
 		EventHotKeyID shortcutIdentifier = {
 			.id = 1,
 			.signature = 'GRSc'
---
+++ Controller/GRStatusItemController.h
@@ -0,0 +1,21 @@
+//
+//  GRStatusItemController.h
+//  Grid
+//
+//  Created by Rob Rix on 11-09-30.
+//  Copyright 2011 Rob Rix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GRStatusItemController : NSObject
+
+@property (nonatomic, assign) IBOutlet NSMenu *statusItemMenu;
+@property (nonatomic, assign, getter=isStatusItemEnabled) BOOL statusItemEnabled;
+
+-(IBAction)openMoreAppsFromDEVONtechnologiesURL:(id)sender;
+-(IBAction)openFacebookURL:(id)sender;
+-(IBAction)openTwitterURL:(id)sender;
+-(IBAction)openLinkedInURL:(id)sender;
+
+@end
