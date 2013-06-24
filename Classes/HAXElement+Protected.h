5f50a9d7b6fca8747be7998de7d21cea706e6725
Scott Perry <github@numist.net>
2013-06-24 14:42:47 -0400

    nonnull attributes as appropriate for HAXElement methods

--- Classes/HAXElement+Protected.h
+++ Classes/HAXElement+Protected.h
@@ -6,15 +6,15 @@
 
 @interface HAXElement ()
 
-+(instancetype)elementWithElementRef:(AXUIElementRef)elementRef;
--(instancetype)initWithElementRef:(AXUIElementRef)elementRef;
++(instancetype)elementWithElementRef:(AXUIElementRef)elementRef __attribute__((nonnull(1)));
+-(instancetype)initWithElementRef:(AXUIElementRef)elementRef __attribute__((nonnull(1)));
 
 @property (nonatomic, readonly) AXUIElementRef elementRef __attribute__((NSObject));
 
--(CFTypeRef)copyAttributeValueForKey:(NSString *)key error:(NSError **)error;
--(bool)setAttributeValue:(CFTypeRef)value forKey:(NSString *)key error:(NSError **)error;
-- (bool)performAction:(NSString *)action error:(NSError **)error;
+-(CFTypeRef)copyAttributeValueForKey:(NSString *)key error:(NSError **)error __attribute__((nonnull(1)));
+-(bool)setAttributeValue:(CFTypeRef)value forKey:(NSString *)key error:(NSError **)error __attribute__((nonnull(1,2)));
+- (bool)performAction:(NSString *)action error:(NSError **)error __attribute__((nonnull(1)));
 
--(id)elementOfClass:(Class)klass forKey:(NSString *)key error:(NSError **)error;
+-(id)elementOfClass:(Class)klass forKey:(NSString *)key error:(NSError **)error __attribute__((nonnull(1,2)));
 
 @end
