615e2b3806d00149459f0c8aac06d3fadf2a01f4
Rob Rix <rob.rix@github.com>
2014-07-04 11:26:27 -0400

    Tweak the colours and stuff.

--- View/GRAreaSelectionView.m
+++ View/GRAreaSelectionView.m
@@ -7,32 +7,29 @@
 @implementation GRAreaSelectionView
 
 -(CGRect)selectedAreaForBounds:(CGRect)bounds {
-	NSRange selectedHorizontalFractionRange = self.delegate.selectedHorizontalFractionRange, selectedVerticalFractionRange = self.delegate.selectedVerticalFractionRange;
-	NSSize fractionSize = NSMakeSize(
+	NSRange horizontal = self.delegate.selectedHorizontalFractionRange, vertical = self.delegate.selectedVerticalFractionRange;
+	NSSize fraction = NSMakeSize(
 		NSWidth(bounds) / self.delegate.selectedHorizontalFraction,
 		NSHeight(bounds) / self.delegate.selectedVerticalFraction
 	);
-	NSRect selectedArea;
-	selectedArea.origin = NSMakePoint(
-		roundf(fractionSize.width * selectedHorizontalFractionRange.location),
-		roundf(fractionSize.height * selectedVerticalFractionRange.location)
-	);
-	selectedArea.size = NSMakeSize(
-		roundf((fractionSize.width * selectedHorizontalFractionRange.location) + (fractionSize.width * selectedHorizontalFractionRange.length) - selectedArea.origin.x),
-		roundf((fractionSize.height * selectedVerticalFractionRange.location) + (fractionSize.height * selectedVerticalFractionRange.length) - selectedArea.origin.y)
-	); // this is done this absurd way to avoid rounding errors (and thus the boundaries of the selection area being drawn one pixel too high or low)
-	return CGRectOffset(selectedArea, bounds.origin.x, bounds.origin.y);
+	NSRect selectedArea = {
+		.origin.x = fraction.width * horizontal.location,
+		.origin.y = fraction.height * vertical.location,
+		.size.width = fraction.width * horizontal.length,
+		.size.height = fraction.height * vertical.length,
+	};
+	return NSIntegralRect(CGRectOffset(selectedArea, bounds.origin.x, bounds.origin.y));
 }
 
 -(void)drawRect:(NSRect)rect {
-	[[NSColor colorWithDeviceWhite: 1.0f alpha: 0.10f] setStroke];
+	[[NSColor colorWithCalibratedWhite:1 alpha:0.1] setStroke];
 	
 	// inactive horizontal fractions
 	NSBezierPath *path = [NSBezierPath bezierPath];
 	for(NSUInteger fraction = 1; fraction <= self.delegate.maximumHorizontalFractions; fraction++) {
 		for(NSUInteger n = 1; n < fraction; n++) {
-			[path moveToPoint: NSMakePoint(roundf((NSWidth(self.bounds) / fraction) * n) + 0.5f, 0)];
-			[path lineToPoint: NSMakePoint(roundf((NSWidth(self.bounds) / fraction) * n) + 0.5f, NSHeight(self.bounds))];
+			[path moveToPoint: NSMakePoint(roundf((NSWidth(self.bounds) / fraction) * n) + 0.5, 0)];
+			[path lineToPoint: NSMakePoint(roundf((NSWidth(self.bounds) / fraction) * n) + 0.5, NSHeight(self.bounds))];
 		}
 	}
 	[path stroke];
@@ -41,40 +38,39 @@
 	path = [NSBezierPath bezierPath];
 	for(NSUInteger fraction = 1; fraction <= self.delegate.maximumVerticalFractions; fraction++) {
 		for(NSUInteger n = 1; n < fraction; n++) {
-			[path moveToPoint: NSMakePoint(0, roundf((NSHeight(self.bounds) / fraction) * n) + 0.5f)];
-			[path lineToPoint: NSMakePoint(NSWidth(self.bounds), roundf((NSHeight(self.bounds) / fraction) * n) + 0.5f)];
+			[path moveToPoint: NSMakePoint(0, roundf((NSHeight(self.bounds) / fraction) * n) + 0.5)];
+			[path lineToPoint: NSMakePoint(NSWidth(self.bounds), roundf((NSHeight(self.bounds) / fraction) * n) + 0.5)];
 		}
 	}
 	[path stroke];
 	
-	
-	[[[NSColor colorForControlTint: [NSColor currentControlTint]] blendedColorWithFraction: 0.1f ofColor: [NSColor clearColor]] setFill];
 	[[NSColor whiteColor] setStroke];
 	
 	// active horizontal fractions
 	path = [NSBezierPath bezierPath];
 	for(NSUInteger n = 1; n < self.delegate.selectedHorizontalFraction; n++) {
-		[path moveToPoint: NSMakePoint(roundf((NSWidth(self.bounds) / self.delegate.selectedHorizontalFraction) * n) + 0.5f, 0)];
-		[path lineToPoint: NSMakePoint(roundf((NSWidth(self.bounds) / self.delegate.selectedHorizontalFraction) * n) + 0.5f, NSHeight(self.bounds))];
+		[path moveToPoint:(NSPoint){ .x = roundf((NSWidth(self.bounds) / self.delegate.selectedHorizontalFraction) * n) + 0.5 }];
+		[path lineToPoint:(NSPoint){ .x = roundf((NSWidth(self.bounds) / self.delegate.selectedHorizontalFraction) * n) + 0.5, .y = NSHeight(self.bounds) }];
 	}
 	[path stroke];
 	
 	// active vertical fractions
 	path = [NSBezierPath bezierPath];
 	for(NSUInteger n = 1; n < self.delegate.selectedVerticalFraction; n++) {
-		[path moveToPoint: NSMakePoint(0, roundf((NSHeight(self.bounds) / self.delegate.selectedVerticalFraction) * n) + 0.5f)];
-		[path lineToPoint: NSMakePoint(NSWidth(self.bounds), roundf((NSHeight(self.bounds) / self.delegate.selectedVerticalFraction) * n) + 0.5f)];
+		[path moveToPoint:(NSPoint){ .y = roundf((NSHeight(self.bounds) / self.delegate.selectedVerticalFraction) * n) + 0.5}];
+		[path lineToPoint:(NSPoint){ .x = NSWidth(self.bounds), .y = roundf((NSHeight(self.bounds) / self.delegate.selectedVerticalFraction) * n) + 0.5}];
 	}
-	[path stroke];
-	
-	// selected area
-	CGRect selectedArea = [self selectedAreaForBounds: self.bounds];
-	path = [NSBezierPath bezierPathWithRect: NSOffsetRect(selectedArea, 0.5f, 0.5f)];
-	[path fill];
 	[path stroke];
 	
 	// view bounds
 	[[NSBezierPath bezierPathWithRect: NSInsetRect(self.bounds, 0.5, 0.5)] stroke];
+	
+	// selected area
+	[[[NSColor alternateSelectedControlColor] colorWithAlphaComponent:0.7] set];
+	CGRect selectedArea = [self selectedAreaForBounds: self.bounds];
+	path = [NSBezierPath bezierPathWithRect: NSOffsetRect(selectedArea, 0.5f, 0.5f)];
+	[path fill];
+	[path stroke];
 }
 
 @end
